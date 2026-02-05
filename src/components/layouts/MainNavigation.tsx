57d894a5978f7f333f1e2e90bf266a0cc70ef0a2
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2026-02-05 19:24:38 +0630

    update nav

--- src/components/layouts/MainNavigation.tsx
+++ src/components/layouts/MainNavigation.tsx
@@ -10,22 +10,54 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
-import {
-  CircleAlertIcon,
-  CircleCheckIcon,
-  CircleDashedIcon,
-} from "lucide-react";
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "/docs/primitives/scroll-area",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "/docs/primitives/tabs",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "/docs/primitives/tooltip",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+]
 
 function MainNavigation() {
   return (
-    <div className="flex items-center justify-between w-full">
+    <div className="flex w-full items-center justify-between">
       <Link to="/">Home</Link>
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="w-96 p-2">
+              <ul className="w-96">
                 <ListItem href="/docs" title="Introduction">
                   Re-usable components built with Tailwind CSS.
                 </ListItem>
@@ -38,50 +70,22 @@ function MainNavigation() {
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>With Icon</NavigationMenuTrigger>
+          <NavigationMenuItem className="hidden md:flex">
+            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid w-[220px] gap-1 p-2">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link
-                      to="#"
-                      className="hover:bg-muted flex items-center gap-2 rounded-md p-2"
-                    >
-                      <CircleAlertIcon className="h-4 w-4" />
-                      Backlog
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link
-                      to="#"
-                      className="hover:bg-muted flex items-center gap-2 rounded-md p-2"
-                    >
-                      <CircleDashedIcon className="h-4 w-4" />
-                      To Do
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link
-                      to="#"
-                      className="hover:bg-muted flex items-center gap-2 rounded-md p-2"
-                    >
-                      <CircleCheckIcon className="h-4 w-4" />
-                      Done
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
+              <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
-
           <NavigationMenuItem>
             <NavigationMenuLink
               asChild
@@ -100,15 +104,12 @@ function ListItem({
   title,
   children,
   href,
-}: {
-  title: string;
-  href: string;
-  children: React.ReactNode;
-}) {
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
   return (
-    <li>
+    <li {...props}>
       <NavigationMenuLink asChild>
-        <Link to={href} className="hover:bg-muted block rounded-md p-2">
+        <Link href={href}>
           <div className="flex flex-col gap-1 text-sm">
             <div className="leading-none font-medium">{title}</div>
             <div className="text-muted-foreground line-clamp-2">{children}</div>
