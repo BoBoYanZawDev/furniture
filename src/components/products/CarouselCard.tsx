5a18df1ea67b2523b6f9f3fd33b15173969fec7b
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2026-03-13 19:28:40 +0630

    Update production description

--- src/components/products/CarouselCard.tsx
+++ src/components/products/CarouselCard.tsx
@@ -29,7 +29,7 @@ export default function CarouselCard({ products }: ProductProps) {
       <CarouselContent className="-ml-1">
         {products.map((product) => (
           <CarouselItem key={product.id} className="pl-1 lg:basis-1/3">
-            <div className="flex p-4 lg:px-4 gap-4">
+            <div className="flex gap-4 p-4 lg:px-4">
               <img
                 src={product.images[0]}
                 alt={product.name}
@@ -37,8 +37,15 @@ export default function CarouselCard({ products }: ProductProps) {
               />
               <div>
                 <h3 className="text-sm font-bold">{product.name}</h3>
-                <p className="text-sm my-2 text-gray-600">{product.description}</p>
-                <Link to={`/products/${product.id}`} className="text-sm font-semibold text-own hover:underline">Read More</Link>
+                <p className="my-2 line-clamp-2 text-sm text-gray-600">
+                  {product.description}
+                </p>
+                <Link
+                  to={`/products/${product.id}`}
+                  className="text-own text-sm font-semibold hover:underline"
+                >
+                  Read More
+                </Link>
               </div>
             </div>
           </CarouselItem>
