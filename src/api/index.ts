8adef9bc0f70c7c4ab36af3dff0cd29c1dae713e
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2026-05-31 19:25:32 +0630

    Update api lib

---
+++ src/api/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
